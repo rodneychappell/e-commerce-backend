b74016545a7f64d5489009b4d4fc6ad419691684
Rodney chappell <tookie@Rodneys-Mini.attlocal.net>
2022-03-16 14:04:32 -0400

    commiting fixing models

--- Develop/models/Product.js
+++ Develop/models/Product.js
@@ -10,6 +10,33 @@ class Product extends Model {}
 Product.init(
   {
     // define columns
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true
+    },
+    
+    product_name: {
+      type: DataTypes.STRING,
+      allowNull: false
+    },
+    stock: {
+      type: DataTypes.DECIMAL,
+      allowNull: false,
+      validate: {
+        isNumeric: true,
+      }
+    },
+    category_id: {
+      type: DataTypes.INTEGER,
+      allowNull: true,
+      references: {
+        model: 'category',
+        key: 'id'
+      }
+    }
+    
   },
   {
     sequelize,
@@ -20,4 +47,4 @@ Product.init(
   }
 );
 
-module.exports = Product;
+module.exports = Produ
